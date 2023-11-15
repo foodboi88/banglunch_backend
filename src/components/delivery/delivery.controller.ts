bae5bc7193486f0fd181a00abd71c6e86c0a0cea
Trung Hieu Do <92080459+foodboi88@users.noreply.github.com>
2023-11-15 23:43:36 +0700

    caculate fee api

--- src/components/delivery/delivery.controller.ts
+++ src/components/delivery/delivery.controller.ts
@@ -13,28 +13,39 @@ export class DeliveryController extends Controller {
      * @returns {Promise<any>} 200 - Return message and status
      * @returns {Promise<any>} 400 - Return error message
      */
-    @Security('jwt', ['user','seller'])
+    @Security('jwt', ['user', 'seller'])
     @Post('/caculate-shipping-cost')
-    public async caculateShippingCost(@Request() request:any, @Body() body: ICaculateShippingCostInput): Promise<any> {
+    public async caculateShippingCost(@Request() request: any, @Body() body: ICaculateShippingCostInput): Promise<any> {
         try {
+            
             const response = await axios.post('https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee', {
-                "service_id":53321,
-                "insurance_value":500000,
+                "service_type_id": 5,
+                "from_district_id": 1442,
+                "to_district_id": 1820,
+                "to_ward_code": "030712",
+                "height": 20,
+                "length": 30,
+                "weight": 3000,
+                "width": 40,
+                "insurance_value": 0,
                 "coupon": null,
-                "from_district_id":1542,
-                "to_district_id":1444,
-                "to_ward_code":"20314",
-                "height":15,
-                "length":15,
-                "weight":1000,
-                "width":15
-            },{
+                "items": [
+                    {
+                        "name": "TEST1",
+                        "quantity": 1,
+                        "height": 200,
+                        "weight": 1000,
+                        "length": 200,
+                        "width": 200
+                    }
+                ]
+            }, {
                 headers: {
-                  'Token': `ee813073-822e-11ee-96dc-de6f804954c9`,
-                  'ShopId': `4695506`,
-                  'Content-Type': 'application/json'
+                    'Token': `ee813073-822e-11ee-96dc-de6f804954c9`,
+                    'ShopId': `4695506`,
+                    'Content-Type': 'application/json'
                 }
-              })
+            })
             return response.data
         } catch (error) {
             this.setStatus(500);
