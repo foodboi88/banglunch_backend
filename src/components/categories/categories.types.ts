11000c1d7863c129f388d0f46efcfcb415316912
Trung Hieu Do <92080459+foodboi88@users.noreply.github.com>
2024-05-12 16:05:25 +0700

    dạy viết component

--- src/components/categories/categories.types.ts
+++
@@ -1,9 +0,0 @@
-export interface ICategory {
-    name: string;
-    description: string;
-}
-
-export interface ICategoryDB extends ICategory {
-    id: string;
-}
-
--- src/components/order-details/order-details.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Route, Tags, Post, Body, Get, Request, Security, Put, Query, Path, Delete, UploadedFiles, UploadedFile, FormField, Example } from "tsoa";
-
-@Route("order-details")
-@Tags("OrderDetails")
-export class OrderDetailsController extends Controller {
-
-}
